42fe3c3ac01d5f75e0ccec7beb106f86da9318f5
Denny Alquinta <dralquinta@google.com>
2024-10-03 09:40:55 -0300

    Reverting changes on front end

--- system-1/main.tf
+++ system-1/main.tf
@@ -16,5 +16,4 @@ module "gce_disk" {
   disk_size_gb = var.disk_size_gb
   zone         = var.zone
   instance     = module.gce_instance.instance_self_link
-  instance_id  = module.gce_instance.instance_self_link
 }
