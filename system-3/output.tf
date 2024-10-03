9c49492a63dd656e0275f972b6593a1a33663bfd
Denny Alquinta <dralquinta@google.com>
2024-10-03 10:29:04 -0300

    Fixing outputs

--- system-3/output.tf
+++ system-3/output.tf
@@ -1,8 +1,11 @@
-# Output the instance's external IP
-output "instance_ip" {
-  value = module.gce_instance.instance_external_ip
+# Output the self-links of the created instances
+output "instance_self_links" {
+  description = "Self-links of all created instances"
+  value       = module.gce_instance[*].instance_self_link  # Output for all instances
 }
 
-output "mount_command" {
-  value = module.gce_disk.disk_mount_command
+# Output the disk names for all instances
+output "disk_names" {
+  description = "Names of all created disks"
+  value       = module.gce_disk[*].disk_name  # Output for all disks
 }
