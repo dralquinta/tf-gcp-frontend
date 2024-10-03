f4f22ae359514223ae9ae610149e97a93b275ea4
Denny Alquinta <dralquinta@google.com>
2024-10-03 08:48:52 -0300

    Fixing branch reference

--- system-1/main.tf
+++ system-1/main.tf
@@ -1,6 +1,6 @@
 # Module to create a GCE instance (Referenced from a GitHub repo)
 module "gce_instance" {
-  source = "git::https://github.com/dralquinta/tf-gcp-compute.git?ref=1-enter-logic-to-format-disks-attached-to-machine" # Replace with the correct URL
+  source = "git::https://github.com/dralquinta/tf-gcp-compute.git?ref=main" # Replace with the correct URL
   instance_name = var.instance_name
   machine_type  = var.machine_type
   zone          = var.zone
@@ -10,7 +10,7 @@ module "gce_instance" {
 
 # Module to create and attach disk (Referenced from a GitHub repo, can be used later)
 module "gce_disk" {
-  source = "git::https://github.com/dralquinta/tf-gcp-disk.git?ref=main" # Replace with the correct URL
+  source = "git::https://github.com/dralquinta/tf-gcp-disk.git?ref==1-enter-logic-to-format-disks-attached-to-machine" # Replace with the correct URL
   depends_on = [ module.gce_instance ]
   disk_name    = var.disk_name
   disk_size_gb = var.disk_size_gb
