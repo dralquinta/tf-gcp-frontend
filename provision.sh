f49b2c876791f2b63159db5d87e27059b9e7910e
Denny Alquinta <dralquinta@google.com>
2024-10-03 08:33:04 -0300

    Adding provision and destroy scripting logic

---
+++ provision.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo "Usage: ./provision.sh system-name"
+  exit 1
+fi
+
+
+cd $1;
+rm -rf .terraform
+terraform init;
+terraform plan --var-file=$1.tfvars.sample
+terraform apply --var-file=$1.tfvars.sample --auto-approve
